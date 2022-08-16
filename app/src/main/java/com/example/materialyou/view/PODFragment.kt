52635fde8a3ff9b63dd1b089ce813df289089253
Rodion Usmanov <RQDerz@yandex.ru>
2022-08-16 23:00:22 +0300

    1 ДЗ. Добавил загрузочный экран во время получения картинки с сервера.

--- app/src/main/java/com/example/materialyou/view/PODFragment.kt
+++ app/src/main/java/com/example/materialyou/view/PODFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.load
+import com.example.materialyou.R
 import com.example.materialyou.databinding.PodFragmentBinding
 import com.example.materialyou.viewmodel.PODViewModel
 import com.example.materialyou.viewmodel.PODViewModelAppState
@@ -52,11 +55,18 @@ class PODFragment : Fragment() {
     }
 
     private fun renderData(podViewModelAppState: PODViewModelAppState?) {
-
+        val loadingScreen = requireActivity().findViewById<FrameLayout>(R.id.loading_screen)
         when (podViewModelAppState) {
-            is PODViewModelAppState.Error -> {}
-            PODViewModelAppState.Loading -> {}
+            is PODViewModelAppState.Error -> {
+                loadingScreen.visibility = View.GONE
+                Toast.makeText(requireContext(), "сломался, не отработал", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            PODViewModelAppState.Loading -> {
+                loadingScreen.visibility = View.VISIBLE
+            }
             is PODViewModelAppState.Success -> {
+                loadingScreen.visibility = View.GONE
                 with(binding) {
                     podImageView.load(podViewModelAppState.hdurl)
                 }
