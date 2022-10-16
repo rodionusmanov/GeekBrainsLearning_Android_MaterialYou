36a56061253bfc420f4db1458cf474d77e271694
Rodion Usmanov <RQDerz@yandex.ru>
2022-10-16 12:49:40 +0300

    3 ДЗ. Добавил иконки на вкладки tabLayout.

--- app/src/main/java/com/example/materialyou/view/navigation/ViewPagerActivity.kt
+++ app/src/main/java/com/example/materialyou/view/navigation/ViewPagerActivity.kt
@@ -2,12 +2,15 @@ package com.example.materialyou.view.navigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.materialyou.R
 import com.example.materialyou.databinding.ViewPagerActivityBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class ViewPagerActivity : AppCompatActivity() {
     private lateinit var binding: ViewPagerActivityBinding
     private val tabTitles = arrayOf("Earth", "Mars", "Solar system")
+    private val tabIcons = arrayOf(R.drawable.earth_icon, R.drawable.mars_icon, R.drawable.solar_system_icon)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ViewPagerActivityBinding.inflate(layoutInflater)
@@ -15,6 +18,7 @@ class ViewPagerActivity : AppCompatActivity() {
         binding.viewPager.adapter = ViewPagerAdapter(this)
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             tab.text = tabTitles[position]
+            tab.icon = ContextCompat.getDrawable(this@ViewPagerActivity, tabIcons[position])
         }.attach()
     }
 }
