9f586a89b2b9405e610d5f0b282b28cccbb61c36
Rodion Usmanov <RQDerz@yandex.ru>
2022-11-09 22:09:36 +0300

    8 ДЗ. Добавил анимацию на SplashScreen.

--- app/src/main/java/com/example/materialyou/splash/SplashActivity.kt
+++ app/src/main/java/com/example/materialyou/splash/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.PersistableBundle
+import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import com.example.materialyou.MainActivity
 import com.example.materialyou.databinding.ActivitySplashLayoutBinding
@@ -13,13 +14,36 @@ class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashLayoutBinding
     private val handler = Handler(Looper.getMainLooper())
-    private val splashDelay = 2000L
+    private val splashDelay = 4000L
+
+    private val scaleIVValue = 0.9f
+    private val scaleTVValue = 1.1f
+    private val rotationValue = 10f
+    private val interpolatorDuration = 5000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySplashLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.splashIv.animate().apply {
+            scaleX(scaleIVValue)
+                .setInterpolator(LinearInterpolator()).duration = interpolatorDuration
+            scaleY(scaleIVValue)
+                .setInterpolator(LinearInterpolator()).duration = interpolatorDuration
+            rotationBy(rotationValue)
+                .setInterpolator(LinearInterpolator()).duration = interpolatorDuration
+        }
+
+        binding.splashTv.animate().apply {
+            scaleX(scaleTVValue)
+                .setInterpolator(LinearInterpolator()).duration = interpolatorDuration
+            scaleY(scaleTVValue)
+                .setInterpolator(LinearInterpolator()).duration = interpolatorDuration
+            rotationBy(-rotationValue)
+                .setInterpolator(LinearInterpolator()).duration = interpolatorDuration
+        }
 
         handler.postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
